6ca259043a5299fe614749a1bdc73c80d460fc95
Chris <chris.l.stew@gmail.com>
2019-02-02 08:01:22 -0800

    updates

--- SINS_game_generator.py
+++ SINS_game_generator.py
@@ -3,25 +3,41 @@ import numpy as np
 vasari = {}
 
 vasari['frigates'] = [
-'Ravastra Skirmisher',
-'Kanrak Assailant',
-'Karrastra Destructor',
-'Junsurak Sentinel',
-'Lasurak Transporter',
-'Stilakus Subverter',
-'Serevun Overseer',
-'Skarovas Enforcer',
-'Sivuskras Ruiner',
-'Orkulus Starbase'
+    'Ravastra Skirmisher',
+    'Kanrak Assailant',
+    'Karrastra Destructor',
+    'Junsurak Sentinel',
+    'Lasurak Transporter',
+    'Stilakus Subverter',
+    'Serevun Overseer',
+    'Skarovas Enforcer',
+    'Sivuskras Ruiner',
+    'Orkulus Starbase'
 ]
 
-vasari['capitals']
+vasari['capitals'] = [
+    'Kortul Devastator',
+    'Skirantra Carrier',
+    'Jarrasul Evacuator',
+    'Antorak Marauder',
+    'Vulkoras Desolator'
+]
 
+vasari['logistics'] = [
+    'Trade Port',
+    'Media Hub',
+    'Matter Processor'
+]
 
-vasari['logistics']
-
-
-vasari['defense']
+vasari['tactical'] = [
+    'Missile Platform',
+    'Hangar Defense',
+    'Regeneration Bay',
+    'Phase Jump Inhibitor',
+    'Nano Weapon Jammer',
+    'Phase Stabilizer',
+    'Kostura Cannon'
+]
 
 
 
