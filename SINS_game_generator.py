5b72ce7cfb644bcf81f34f4aff68256d7b931d0e
chrislstewart <chris.l.stew@gmail.com>
2019-02-02 07:52:09 -0800

    SINS of a Solar Empire random game settings generation script

---
+++ SINS_game_generator.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+vasari = {}
+
+vasari['frigates'] = [
+'Ravastra Skirmisher',
+'Kanrak Assailant',
+'Karrastra Destructor',
+'Junsurak Sentinel',
+'Lasurak Transporter',
+'Stilakus Subverter',
+'Serevun Overseer',
+'Skarovas Enforcer',
+'Sivuskras Ruiner',
+'Orkulus Starbase'
+]
+
+vasari['capitals']
+
+
+vasari['logistics']
+
+
+vasari['defense']
+
+
+
+
+
+
+
